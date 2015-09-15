48d583de721e20e40b3f2b267f7cd66bad055101
Mario Konrad <mario.konrad@gmx.net>
2015-09-15 10:32:01 +0200

    Separation of header and implementation of topological sort

---
+++ src/graph/graph_toposort.cpp
@@ -0,0 +1,57 @@
+#include "graph_toposort.hpp"
+
+namespace graph
+{
+/// This algorithm adds the vertices of the specified matrix
+/// to the vertex list in topological order. The graph must
+/// be free of cycles to determine a topological order.
+///
+/// Complexity: O(V + E)
+///
+/// \param[in] m The adjacency matrix.
+/// \return A tuple containing the list and a status which is:
+///   - \c true : sorting successful
+///   - \c false : graph contains cycles
+///
+std::tuple<adjmatrix::vertex_list, bool> topological_sort(const adjmatrix & m)
+{
+	// copy of matrix to work on (remove edges)
+	adjmatrix tm(m);
+
+	// all nodes with no incoming edges
+	adjmatrix::vertex_list Q;
+	for (adjmatrix::vertex_t i = 0; i < m.size(); ++i)
+		if (tm.num_incoming(i) == 0)
+			Q.push_back(i);
+
+	adjmatrix::vertex_list v;
+
+	// sort
+	while (Q.size() > 0) {
+		// remove node from Q
+		adjmatrix::vertex_t node = Q.front();
+		Q.erase(Q.begin());
+
+		// node is member of the result vector
+		v.push_back(node);
+
+		for (adjmatrix::vertex_t i = 0; i < tm.size(); ++i) {
+			if (i == node)
+				continue; // not to self
+			if (tm.edge(node, i)) {
+				tm.remove(node, i); // remove edge from graph
+
+				// has i other incoming edges?
+				if (tm.num_incoming(i) == 0)
+					Q.push_back(i);
+			}
+		}
+	}
+
+	// cycle / remaining edges?
+	if (tm.num_edges() > 0)
+		return std::make_tuple(adjmatrix::vertex_list{}, false);
+
+	return std::make_tuple(v, true);
+}
+}
